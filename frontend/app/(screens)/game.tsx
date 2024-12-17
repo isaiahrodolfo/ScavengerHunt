ca7049167621d8253c96a6be39982a7395bf59b7
Isaiah <irodol@uw.edu>
2024-12-16 19:56:43 -0800

    Add camera

--- frontend/app/(screens)/game.tsx
+++ frontend/app/(screens)/game.tsx
@@ -1,10 +1,15 @@
+import { CameraView, CameraType, useCameraPermissions } from 'expo-camera';
 import { router, useLocalSearchParams } from 'expo-router';
 import React, { useEffect, useState } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Button, TouchableOpacity, ActivityIndicator } from 'react-native';
 
 export default function GameScreen() {
   const { roomCode, isHost } = useLocalSearchParams();
-  const [timer, setTimer] = useState(2);
+  const [timer, setTimer] = useState(30);
+
+  // State variables for camera
+  const [facing, setFacing] = useState<CameraType>('back');
+  const [permission, requestPermission] = useCameraPermissions();
 
   useEffect(() => {
     const interval = setInterval(() => {
@@ -22,15 +27,70 @@ export default function GameScreen() {
     return () => clearInterval(interval);
   }, [timer]);
 
+  if (!permission) {
+    // Camera permissions are still loading.
+    return <ActivityIndicator size={'large'} />;
+  }
+
+  if (!permission.granted) {
+    // Camera permissions are not granted yet.
+    return (
+      <View style={styles.container}>
+        <Text style={styles.message}>We need your permission to show the camera</Text>
+        <Button onPress={requestPermission} title="grant permission" />
+      </View>
+    );
+  }
+
+  function toggleCameraFacing() {
+    setFacing(current => (current === 'back' ? 'front' : 'back'));
+  }
+
   return (
     <View style={styles.container}>
       <Text>{roomCode}</Text>
       <Text style={styles.timer}>{timer}</Text>
+      <CameraView style={styles.camera} facing={facing}>
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity style={styles.button} onPress={toggleCameraFacing}>
+            <Text style={styles.text}>Flip Camera</Text>
+          </TouchableOpacity>
+        </View>
+      </CameraView>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: { flex: 1, justifyContent: 'center', alignItems: 'center' },
-  timer: { fontSize: 50 },
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  timer: {
+    fontSize: 50
+  },
+  message: {
+    textAlign: 'center',
+    paddingBottom: 10,
+  },
+  camera: {
+    flex: 1,
+  },
+  buttonContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    backgroundColor: 'transparent',
+    margin: 64,
+  },
+  button: {
+    flex: 1,
+    alignSelf: 'flex-end',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: 'white',
+  },
 });
