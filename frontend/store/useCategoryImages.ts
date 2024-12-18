3fe9e764ba4ea212c4f16c11297599e3cfb2df58
Isaiah <irodol@uw.edu>
2024-12-17 16:49:38 -0800

    Add Zustand state for selected image, category images

---
+++ frontend/store/useCategoryImages.ts
@@ -0,0 +1,37 @@
+import { Category } from '@/types/game';
+import { create } from 'zustand'
+import { devtools } from 'zustand/middleware'
+
+interface CategoryImages {
+  categoryImages: Category[];
+  setCategoryImages: (imageUri: string, toCategoryIndex: number, toImageIndex?: number) => void;
+}
+
+export const useCategoryImages = create<CategoryImages>()(
+  devtools((set) => ({
+    categoryImages: [
+      { images: [] },
+      { images: [] },
+      { images: [] },
+      { images: [] },
+    ],
+    setCategoryImages: (imageUri, toCategoryIndex, toImageIndex) =>
+      set((state) => {
+        // Destructure the relevant category
+        const updatedCategories = [...state.categoryImages];
+        const category = updatedCategories[toCategoryIndex];
+
+        // Add the new image at the specified index or append if no index provided
+        if (category) {
+          const newImageUri = imageUri; // Replace with the actual image URI
+          if (typeof toImageIndex === 'number' && toImageIndex >= 0) {
+            category.images.splice(toImageIndex, 0, newImageUri);
+          } else {
+            category.images.push(newImageUri);
+          }
+        }
+
+        return { categoryImages: updatedCategories };
+      }),
+  }))
+);
